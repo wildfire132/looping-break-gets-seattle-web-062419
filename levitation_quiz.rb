e5ad7a5ba587e857cb3b1a9605746ebccbd82a0a
Katana Tran <tkatyk21@gmail.com>
2019-06-17 05:09:38 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,7 @@
 
 def levitation_quiz(answer)
-	gets answer == 'Wingardium Leviosa'
+	gets answer
+	== 'Wingardium Leviosa'
 	  puts "What is the spell that enacts levitation?"
 	puts "You passed the quiz!"
 end
